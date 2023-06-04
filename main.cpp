4bc186711346c0baf554dc657bf80b2969ee24f5
bekhungyi <bekhungyi22@gmail.com>
2023-06-04 18:45:57 +0800

    bingbong

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Input N: ";
+    cin >> n;
+
+    int temp = n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout << " ";
+        }
+        temp--;
+        for (int j = 1; j == i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j < i+1; j++)
+        {
+            cout << "x";
+        }
+        cout << endl;
+    }
+}
